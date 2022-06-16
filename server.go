34534a2d40d6e25ff5657ba6b365e548262af385
sutsuk <utsu017@outlook.jp>
2022-06-16 17:47:25 +0900

    Update server.go

--- server.go
+++ server.go
@@ -8,23 +8,34 @@ type text struct {
   Pass string
 }
 
+// showPage: Show and edit a web page.
 func showPage(wrt http.ResponseWriter, req *http.Request){
-  var fname string = "." + req.URL.Path // <current directory> + <string following the domain>
+  // fname: <current directory> + <string following the domain>
+  var fname string = "." + req.URL.Path
+  
+  // pass: Editing function passcode
   var pass string = "passcode"
+  
+  // Get all received http.Request parameters
   req.ParseForm()
   body, err := ioutil.ReadFile(fname)
   if err != nil {
+    // Web page is not exist
     fmt.Fprintf(wrt, "Bad Request: Not Exist")
     return
   }
   for key, value := range req.Form {
     if key == "pass" {
+      // strings.Join: ([]string, string) -> string
       if strings.Join(value, "") == pass {
-        temp, _ := template.ParseFiles("edit.html");
+        temp, _ := template.ParseFiles("edit.html")
+        
+        // temp.Execute: Apply (Body, Name, Pass) to edit.html.
         temp.Execute(wrt, &text{Body:body, Name:req.URL.Path, Pass:pass})
         return
       }
     }else if key == "save" {
+      // strings.Join: ([]string, string) ->  string
       if strings.Join(value, "") == pass {
         saveText(wrt, req)
       }
@@ -33,14 +44,23 @@ func showPage(wrt http.ResponseWriter, req *http.Request){
   fmt.Fprintf(wrt, "%s", body)
 }
 
+// saveText: Save the sent text data.
 func saveText(wrt http.ResponseWriter, req *http.Request){
+  // Get the sent data under the name "txt_body"
   body := req.FormValue("txt_body")
+  
+  // ioutil.WriteFile: Save data in <current directory> + <string following the domain>
   ioutil.WriteFile("." + req.URL.Path, []byte(body), 0600)
+  
+  // http.Redirect: Redirect to the saved web page with a status code.
   http.Redirect(wrt, req, req.URL.Path, http.StatusFound)
 } 
 
 func main(){
+  // http.HandleFunc: Triggered by access to "/", execute the showPage function.
   http.HandleFunc("/", showPage)
   println("Running...")
+  
+  // http.ListenAndServe: Monitor port 8080
   http.ListenAndServe(":8080", nil)
 }
